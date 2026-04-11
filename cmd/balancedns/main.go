98b86538c511ab7d90f6b0250955f49a1ed6765e
astracat <astracat@MacBook-Air-astracat.local>
2026-04-11 23:32:12 +0300

    fix: include cmd entrypoint in repository

---
+++ cmd/balancedns/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"balancedns/internal/app"
+	"balancedns/internal/config"
+)
+
+func main() {
+	configPath := flag.String("config", "configs/balancedns.lua", "Path to Lua config")
+	flag.Parse()
+
+	cfg, err := config.Load(*configPath)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "config error: %v\n", err)
+		os.Exit(1)
+	}
+
+	srv, err := app.New(cfg)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "startup error: %v\n", err)
+		os.Exit(1)
+	}
+
+	ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
+	defer stop()
+
+	if err := srv.Run(ctx); err != nil {
+		fmt.Fprintf(os.Stderr, "runtime error: %v\n", err)
+		os.Exit(1)
+	}
+}
